95c77cc604da56a502ea1da456fe0a349af473f1
Elizabeth Bissell <wynnebissell@gmail.com>
2025-02-24 15:10:48 -0700

    added functions bulk density and factor of safety

---
+++ functions.py
@@ -0,0 +1,32 @@
+import math
+
+def bulk_density (pg, n, pw=997):
+    """This function calculates the bulk density of a saturated material
+    this function takes the arguments
+     - density of grains (pg)
+     - porosity of grains (n)
+     - density of liquid (pw) #if not specified assumed water density 997 kg/m³
+     to return the bulk density of a saturated porous material (pb)
+     """
+    pb = pg (1-n) + pw(n)
+    return pb
+
+def factor_of_safety (c, pb, h, theta, pw=997, wt, tan_phi):
+    """This function assesses the stability of a hill slope,
+    by looking at the ratio of resisting stresses to driving stress
+    this function takes arguments:
+     - cohesion (c) #mineral or biological
+     - bulk density of the material (pb),
+     - height of hill slope (h)
+     - angle of the hill slope (theta),
+     - density of liquid (pw) #if not specified assumed water density 997 kg/m³
+     - water table height above failure plane (wt)
+     - internal angle of friction (tan_phi)
+     - acceleration due to gravity at the Earth's surface is 9.8 (m/s²)
+     and assesses the stability of the hill slope by finding Fs and seeing if it is greater or less than 1
+     """
+    fs= (c+(pb*9.87*h*(math.cos(theta)))-(pw*9.8*wt)*tan_phi)/(pb*9.87*h*math.sin(theta))
+    if fs > 1:
+        print("The factor of safety is", fs ,"therefore the slope should be stable")
+    elif fs < 1:
+        print("The factor of safety is", fs ,"the driving stress is exceeding the resistance and the slope is likely to fail")
---
+++ test_functions.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_bulk_density(self):
+        self.fail()
